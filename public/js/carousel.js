fbc3b7e4ee4bd25a96e0b06374b31b02977a7db9
DESKTOP-I3I4VIC\koozy <koozya96@gmail.com>
2017-08-01 03:07:43 +0300

    Add about, 404; Fix grid system on index, and content to index; Fix header; Fix footer; Fix navbar to top; Add images, fonts;

---
+++ public/js/carousel.js
@@ -0,0 +1,10 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:1,
+    loop:true,
+    margin:10,
+    autoHeight:true,
+    autoplay:true,
+    autoplayTimeout:4000,
+    autoplayHoverPause:true
+});
